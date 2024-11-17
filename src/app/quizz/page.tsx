6e31018bb459e9fde65804e37c6745455ebb7803
Deepak Attri <deepak23attri@gmail.com>
2024-11-17 23:27:32 +0530

    added basics

--- src/app/quizz/page.tsx
+++ src/app/quizz/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1">
     <main className="flex justify-center flex-1">
-      <h1 className="text-6xl font-bold">Welcome to Quiz App👋</h1>
+      <h1 className="text-3xl font-bold">Welcome to Quiz App👋</h1>
     </main>
     <footer className="footer=pb-9 px-5 relative mb-0">
       <Button>Start</Button>
